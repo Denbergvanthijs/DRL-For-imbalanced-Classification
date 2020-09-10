74e94eb0273bc1d8d66cb61c80cb8454948c2cf6
denbergvanthijs <denbergvanthijs@gmail.com>
2020-09-10 10:43:31 +0200

    Improved code quality and speed

--- ICMDP_Env.py
+++ ICMDP_Env.py
@@ -2,12 +2,11 @@ from datetime import datetime
 
 import gym
 import numpy as np
-import tensorflow as tf
 from gym import spaces
 from gym.utils import seeding
-from pandas import unique
+from tensorflow.compat.v1.summary import FileWriter, Summary
 
-from predict import calculate_metrics
+from predict import calculate_metrics, make_predictions
 
 
 class ClassifyEnv(gym.Env):
@@ -22,62 +21,62 @@ class ClassifyEnv(gym.Env):
         self.y_test = y_test
         self.id = np.arange(self.X_train.shape[0])  # List of IDs to connect X and y data
 
-        self.game_len = self.X_train.shape[0]
+        self.X_len = self.X_train.shape[0]
 
-        self.num_classes = unique(self.y_train).size
-        self.action_space = spaces.Discrete(self.num_classes)
-        self.step_ind = 0  # Episode step, resets every episode
-        self.y_pred = []
+        self.action_space = spaces.Discrete(2)  # 2 classes: Minority and majority
+        self.episode_step = 0  # Episode step, resets every episode
 
-        self.writer = tf.compat.v1.summary.FileWriter("./logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S"))
+        self.writer = FileWriter("./logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S"))
         self.step_number = 0  # Global episode number
         self.metrics_interval = metrics_interval  # Interval to update metrics for logging
+        self.model = None
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def step(self, a):
-        self.y_pred.append(a)  # Append by policy predicted answer to y_pred
+    def step(self, action):
+        """
+        The environment takes a step based on action given by policy.
+        Every `metrics_interval`-steps metrics will be logged for Tensorboard.
+        """
         self.step_number += 1
         info = {}
         terminal = False
-        curr_answer = self.y_train[self.id[self.step_ind]]
+        curr_answer = self.y_train[self.id[self.episode_step]]
 
-        if a == curr_answer:  # a: return of policy
-            # When y_pred == y_true
-            if curr_answer == 1:  # Minority
+        if action == curr_answer:  # Correct action
+            if curr_answer:  # Minority
                 reward = 1
-            else:
-                reward = self.imb_rate  # Majority
-        else:
-            # When y_pred != y_true
-            if curr_answer == 1:  # Minority
+            else:  # Majority
+                reward = self.imb_rate
+        else:  # Incorrect action
+            if curr_answer:  # Minority
                 reward = -1
                 if self.mode == "train":
                     terminal = True  # Stop episode when minority class is misclassified
-            else:
-                reward = -self.imb_rate  # Majority
-        self.step_ind += 1
+            else:  # Majority
+                reward = -self.imb_rate
 
-        if self.step_ind == self.game_len - 1:
-            terminal = True
+        self.episode_step += 1
 
         if self.step_number % self.metrics_interval == 0:  # Collect metrics every `metrics_interval`-steps
-            y_pred = [np.argmax(x) for x in self.model.predict(self.X_test)]
+            y_pred = make_predictions(self.model, self.X_test)
             info = calculate_metrics(self.y_test, y_pred)
 
             for k, v in info.items():
-                summary = tf.compat.v1.summary.Summary(value=[tf.compat.v1.summary.Summary.Value(tag=k, simple_value=v)])
+                summary = Summary(value=[Summary.Value(tag=k, simple_value=v)])
                 self.writer.add_summary(summary, global_step=self.step_number)
 
-        return self.X_train[self.id[self.step_ind]], reward, terminal, info
+        if self.episode_step == self.X_len - 1:
+            terminal = True
+
+        return self.X_train[self.id[self.episode_step]], reward, terminal, info
 
     def reset(self):
         """returns: (states, observations)."""
         if self.mode == "train":
             np.random.shuffle(self.id)
 
-        self.step_ind = 0
-        self.y_pred = []
-        return self.X_train[self.id[self.step_ind]]
+        self.episode_step = 0  # Reset episode step counter at the end of every episode
+        return self.X_train[self.id[self.episode_step]]
