58d2ec8d016f025414d8a6812463b792ed6832a1
denbergvanthijs <denbergvanthijs@gmail.com>
2020-09-28 13:15:41 +0200

    Added loss to callback

--- callbacks.py
+++ callbacks.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import numpy as np
 from tensorflow.compat.v1.summary import FileWriter, Summary
 from tensorflow.keras.callbacks import Callback
 
@@ -16,14 +17,22 @@ class Metrics(Callback):
         self.FP_bound = FP_bound
         self.save_after = save_after
         self.writer = FileWriter(f"./logs/{datetime.now().strftime('%Y%m%d_%H%M%S')}")
+        self.loss = []
 
     def on_step_end(self, episode_step, logs):
         """Calculate metrics every `interval`-steps. Save target_model if conditions are met."""
         self.step += 1
+        self.loss.append(logs.get("metrics")[0])
 
         if not self.step % self.interval:
             y_pred = make_predictions(self.model.target_model, self.X_val)
             stats = calculate_metrics(self.y_val, y_pred)
+
+            if np.isnan(self.loss).all():  # If all entries are NaN, this happens during training
+                stats["loss"] = 0
+            else:
+                stats["loss"] = np.nanmean(self.loss)
+            self.loss = []  # Reset loss every `self.interval`
 
             for k, v in stats.items():
                 summary = Summary(value=[Summary.Value(tag=k, simple_value=v)])
